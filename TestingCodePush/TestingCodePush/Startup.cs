03a2d725242857fe8e712c5806b528016de18f68
saif-ullah <saif_ullah@hotmail.com>
2018-03-05 10:53:11 +0500

    first push

---
+++ TestingCodePush/TestingCodePush/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingCodePush.Startup))]
+namespace TestingCodePush
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
